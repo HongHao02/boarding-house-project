5d2a5ebf46e7cd534d11079476766ef204b1e617
HongHao <honghaocp@gmail.com>
2024-04-13 00:14:59 +0700

    deploy branch

--- src/layouts/components/index.js
+++ src/layouts/components/index.js
@@ -1,4 +1,4 @@
-export { default as Header, default } from './Header';
+export { default, default as Header } from './Header';
 export { default as Sidebar } from './SideBar';
 export { default as RightSidebar } from './RightSidebar';
 export { default as SidebarWithBurgerMenu } from './SidebarWithBurgerMenu';
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,3 +1,3 @@
-export { default as DefaultLayout, default } from './DefaultLayout';
+export { default, default as DefaultLayout } from './DefaultLayout';
 export { default as HeaderOnly } from './HeaderOnly';
 export { default as NoRightSidebar } from './NoRightSidebar';
