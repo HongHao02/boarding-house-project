82c90782e46250bcefca6546d435a56ec0cab24f
HongHao <honghaocp@gmail.com>
2024-05-08 22:29:03 +0700

    fix show contact user list

--- src/layouts/components/RightSidebar/RightSidebar.js
+++ src/layouts/components/RightSidebar/RightSidebar.js
@@ -1,8 +1,10 @@
+import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import ContactUser from '~/components/ContactUser';
 import * as randomServices from '~/services/randomServices';
 
 function RightSidebar() {
+    const { user } = useSelector((state) => state.users);
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
@@ -19,7 +21,7 @@ function RightSidebar() {
         <div className="w-72 ">
             <h2 className="font-bold text-gray-500 my-2 pb-2">Người liên hệ gần đây</h2>
             <div className="flex flex-col gap-y-4 mt-2 border-b-2 border-b-blue-gray-50 pb-4">
-                {users.length > 0 &&
+                {(user!= null &&  (users?.length ?? 0) > 0) &&
                     users.map(({ name, picture }, index) => (
                         <ContactUser key={index} name={name} picture={picture}></ContactUser>
                     ))}
